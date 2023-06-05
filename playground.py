fe10e06ee17ed3f8ffa35f96c5748cf3d891830d
Nikko <xokkinx@gmail.com>
2023-06-05 14:21:11 -0700

    update

--- playground.py
+++ playground.py
@@ -4,10 +4,10 @@ input_year = int(input())
 
 
 def ily(year):
-    if year >= 1000 and year % 400 == 0:
+    if year % 400 == 0:
         print(f"{year} - leap year")
         return True
-    elif input_year % 4 == 0:
+    elif year % 4 == 0 and year % 100 != 0:
         print(f"{year} - leap year")
         return True
     else:

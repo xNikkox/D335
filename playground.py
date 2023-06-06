ee0b2a4835e61da4fe1f1756832933e19cf4aa42
Nikko <xokkinx@gmail.com>
2023-06-05 22:29:18 -0700

    update

--- playground.py
+++ playground.py
@@ -1,18 +1,17 @@
-is_leap_year = False
-
-input_year = int(input())
-
-
-def ily(year):
-    if year % 400 == 0:
-        print(f"{year} - leap year")
-        return True
-    elif year % 4 == 0 and year % 100 != 0:
-        print(f"{year} - leap year")
-        return True
-    else:
-        print(f"{year} - not a leap year")
-        return False
+# As long as x is greater than 0
+# Output x modulo 2 (remainder is either 0 or 1)
+# Assign x with x divided by 2
+x = int(input())
 
 
-ily(input_year)
+def binary(xx):    # function to convert integer to binary
+    while xx > 0:
+        xx = xx // 2
+        # bin() converts integer to binary string and [2:] removes the 0b prefix
+        binary_string = bin(xx)[2:]
+        # reversed() reverses the string
+        reversed_binary = ''.join(reversed(binary_string))
+        return reversed_binary
+
+
+print(binary(x))

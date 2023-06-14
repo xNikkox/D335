dcf31a0ac0665defbfc3d9f640def59e50a77273
Nikko <xokkinx@gmail.com>
2023-06-13 22:20:56 -0700

    update

--- playground.py
+++ playground.py
@@ -1,17 +1,18 @@
-# As long as x is greater than 0
-# Output x modulo 2 (remainder is either 0 or 1)
-# Assign x with x divided by 2
-x = int(input())
+success word = input()
+password = ''
 
+''' Type your code here. '''
+t_char = {
+    "i": 1,
+    "a": "@",
+    "m": "M",
+    "B": 8,
+    "s": "$"
+    }
 
-def binary(xx):    # function to convert integer to binary
-    while xx > 0:
-        xx = xx // 2
-        # bin() converts integer to binary string and [2:] removes the 0b prefix
-        binary_string = bin(xx)[2:]
-        # reversed() reverses the string
-        reversed_binary = ''.join(reversed(binary_string))
-        return reversed_binary
-
-
-print(binary(x))
+def parse_string(string, target_char):
+    result = []
+    for char in string:
+        if char == target_char:
+            result = target_char.replace(t_char.keys, t_char.values)
+            print(result)

04fed9ad58fee34522b61d191d3484ea24bfe77a
Nikko <xokkinx@gmail.com>
2023-06-13 22:21:58 -0700

    update

--- playground.py
+++ playground.py
@@ -16,3 +16,10 @@ def parse_string(string, target_char):
         if char == target_char:
             result = target_char.replace(t_char.keys, t_char.values)
             print(result)
+
+#fix hints
+#1
+#In the line result = target_char.replace(t_char.keys, t_char.values), the replace() method is being used on target_char, which is a single character obtained from the for loop. 
+#owever, replace() is a string method and cannot be directly applied to a single character. It expects a string as the target.
+#2
+#The keys and values in t_char.keys and t_char.values should be functions (t_char.keys() and t_char.values()) to retrieve the keys and values of the t_char dictionary correctly.

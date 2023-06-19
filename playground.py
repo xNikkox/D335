6958062d71f9b484439b99ecf548b64c7bf1ddee
Nikko <xokkinx@gmail.com>
2023-06-18 19:27:09 -0700

    update

--- playground.py
+++ playground.py
@@ -14,13 +14,13 @@ t_char = {
 
 #function to find and replace character
 def parse_string(string, target_char):
-    result = []
+    password = []
     string_list = list(target_char) 
     for i in range(len(string_list)):
         if string_list[i] == t_char.keys[i]:
             string_list[i] = t_char.values[i]
-    
-    
-#convert the list back to a string
-new_string = "".join(string_list)
-print(new_string) 
+            #convert the list back to a string
+            password = "".join(string_list)
+            print(password) 
+
+parse_string(word, password)

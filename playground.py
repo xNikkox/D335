f7ba1192c2b5702cb808b2a7ff97f41d9f08971f
Nikko <xokkinx@gmail.com>
2023-06-18 19:06:30 -0700

    update

--- playground.py
+++ playground.py
@@ -1,7 +1,7 @@
-success word = input()
+word = input()
 password = ''
 
-''' Type your code here. '''
+#character key and value dictionary 
 t_char = {
     "i": 1,
     "a": "@",
@@ -10,16 +10,21 @@ t_char = {
     "s": "$"
     }
 
+#function to find and replace character
 def parse_string(string, target_char):
     result = []
-    for char in string:
-        if char == target_char:
-            result = target_char.replace(t_char.keys, t_char.values)
-            print(result)
-
+    string_list = list(target_char) 
+    for i in range(len(string_list)):
+        if string_list[i] == t_char.keys:
+            string_list[i] = t_char.values
+    
+    
+#convert the list back to a string
+new_string = "".join(string_list)
+print(new_string) 
 #fix hints
 #1
 #In the line result = target_char.replace(t_char.keys, t_char.values), the replace() method is being used on target_char, which is a single character obtained from the for loop. 
-#owever, replace() is a string method and cannot be directly applied to a single character. It expects a string as the target.
+#however, replace() is a string method and cannot be directly applied to a single character. It expects a string as the target.
 #2
 #The keys and values in t_char.keys and t_char.values should be functions (t_char.keys() and t_char.values()) to retrieve the keys and values of the t_char dictionary correctly.

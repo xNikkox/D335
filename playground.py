56dafafa41f62f35c1e3d50e3c32aca60ce7c75c
Nikko <xokkinx@gmail.com>
2023-06-18 19:09:05 -0700

    update

--- playground.py
+++ playground.py
@@ -24,9 +24,3 @@ def parse_string(string, target_char):
 #convert the list back to a string
 new_string = "".join(string_list)
 print(new_string) 
-#fix hints
-#1
-#In the line result = target_char.replace(t_char.keys, t_char.values), the replace() method is being used on target_char, which is a single character obtained from the for loop. 
-#however, replace() is a string method and cannot be directly applied to a single character. It expects a string as the target.
-#2
-#The keys and values in t_char.keys and t_char.values should be functions (t_char.keys() and t_char.values()) to retrieve the keys and values of the t_char dictionary correctly.

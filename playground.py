f57c17e2cadc5a6bd88e4662158b8691a77d94ae
Nikko <xokkinx@gmail.com>
2023-06-18 19:08:40 -0700

    update

--- playground.py
+++ playground.py
@@ -10,13 +10,15 @@ t_char = {
     "s": "$"
     }
 
+
+
 #function to find and replace character
 def parse_string(string, target_char):
     result = []
     string_list = list(target_char) 
     for i in range(len(string_list)):
-        if string_list[i] == t_char.keys:
-            string_list[i] = t_char.values
+        if string_list[i] == t_char.keys[i]:
+            string_list[i] = t_char.values[i]
     
     
 #convert the list back to a string
